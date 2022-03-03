cf50d0e0d204d3ed47ad8bf1aba747074027d151
Soushi Tanaka <wntsssbbsnws@yahoo.co.jp>
2022-03-03 10:58:10 +0900

    initial commit

---
+++ GachaApp/Repository/API/GachaAPI.swift
@@ -0,0 +1,8 @@
+//
+//  GachaAPI.swift
+//  GachaApp
+//
+//  Created by 田中 颯志 on 2022/03/03.
+//
+
+import Foundation
---
+++ GachaApp/UIComponent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GachaApp
+//
+//  Created by 田中 颯志 on 2022/03/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
