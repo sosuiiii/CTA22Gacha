60c8c73ee67c29be145a084f06001f1837ed80bc
Soushi Tanaka <wntsssbbsnws@yahoo.co.jp>
2022-03-05 22:24:41 +0900

    Add RegisterUserViewController

---
+++ GachaApp/UIComponent/ViewController/RegisterUser/RegisterUserViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterUserViewController.swift
+//  GachaApp
+//
+//  Created by 田中 颯志 on 2022/03/05.
+//
+
+import UIKit
+
+class RegisterUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "GachaApp"
+    }
+}
