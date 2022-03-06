6c46e8f33ffe6a6febf45dfbd5f539d2a1de2881
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2022-03-06 23:31:57 +0900

    Add MockGachaAPI

--- GachaApp/Repository/API/GachaAPI.swift
+++ GachaApp/Repository/API/GachaAPI.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import Moya
+import RxSwift
+
+struct MockGacha {
+    let list: [String]
+    
+    init(list: [String]) {
+        self.list = list
+    }
+}
+
+struct MockError: Error {}
+
+final class GachaAPI {
+    private let apiProvider = MoyaProvider<GachaTarget>()
+}
+
+extension GachaAPI: GachaAPIType {
+    func get(result: Result<Void, Error>) -> Single<MockGacha> {
+        return Single<MockGacha>.create { observer in
+            switch result {
+            case .success:
+                observer(.success(MockGacha(list: ["mock", "mock2"])))
+            case .failure:
+                observer(.failure(MockError()))
+            }
+            return Disposables.create()
+        }
+    }
+}
